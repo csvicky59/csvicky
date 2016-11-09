de535d9cd40d3c82f963a792e64f381956558f9c
C S Vignesh <csvicky59@gmail.com>
2016-11-09 12:50:11 +0530

    Add files via upload

---
+++ checkNum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class checkNum{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+	    int num=sc.nextInt();
+	    if(num==0){
+	    	System.out.println("Zero");
+	    }
+	    else if(num<0){
+	    	System.out.println("Negative");
+	    }
+	    else if(num>0){
+	    	System.out.println("Positive");
+	    }
+	}	
+}
